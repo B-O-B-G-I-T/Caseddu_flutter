c67f59a94350f094b3a16fc083c8e63266a79339
jeanForconi <forconijean@gmail.com>
2023-10-28 21:14:50 +0200

    page parametre clean

--- lib/features/auth/data/datasources/authentification_remote_data_source.dart
+++ lib/features/auth/data/datasources/authentification_remote_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../../../core/errors/firebase_exceptions.dart';
 import '../../../../../core/params/params.dart';
